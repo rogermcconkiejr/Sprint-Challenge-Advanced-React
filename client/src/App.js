f01cbbdbcbb1f865b867058c7d270c34f86cc05f
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-06 09:23:50 -0600

    state set and componentDidMount written

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import axios from "./axios";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  state = {
+    playerData: []
+  };
+  componentDidMount(){
+    axios
+      .get('http://localhost:5000/api/players')
+      .then((response) => {
+        console.log('data:', response)
+        this.setState({playerData: response.data})
+      })
+      .catch(err => console.log('error'));
+    }
+  render () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Soccer Data</h1>
     </div>
   );
 }
-
+}
 export default App;
